326770ddb813104a99f08ee7fd5474a5303a2ed3
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-11 17:39:37 -0600

    Created a basic file structure, adding in some test images and also a component called main

---
+++ src/components/Main/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+class Main extends React.Component {
+	render() {
+		return(
+			<div className="block">
+				<h1>Sean</h1>
+				<h1>Belverstone.</h1>
+				<h4>Full Stack Developer.</h4>
+			</div>
+		)
+	}
+}
+
+export default Main;
