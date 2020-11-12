5a9fc4c4afa8161a99cf9da47629c75643e7934e
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-12 17:41:56 -0600

    added css styling to main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,12 +1,13 @@
-import React, {Component} from "react";
+import React from "react";
+import "./style.css";
 
 class Main extends React.Component {
 	render() {
 		return(
 			<div className="block">
-				<h1>Sean</h1>
-				<h1>Belverstone.</h1>
-				<h4>Full Stack Developer.</h4>
+				<h1 className="name">Sean</h1>
+				<h1 className="name">Belverstone.</h1>
+				<h4 id="role">Full Stack Developer.</h4>
 			</div>
 		)
 	}
