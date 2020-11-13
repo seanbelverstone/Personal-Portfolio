ca3515e47e26497b036e67f15caba8e5380c98e9
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-12 19:23:22 -0600

    Created a new component, AboutMe. Added in an image of myself as a test, and changed the style hook to be opacity instead, as i want the content to fade in.

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,22 +1,28 @@
 import React, { useState } from "react";
 import { motion, useAnimation } from "framer-motion";
+import AboutMe from "../AboutMe";
 import "./style.css";
 
 const Main = () => {
-	const [style, setStyle] = useState("none");
+	// Using hooks, creating a state of style to be "none"
+	const [style, setStyle] = useState("0");
 
+
+	// controls allow us to control the animation at will
 	const controls = useAnimation();
 
 	const animateName = () => {
-		setTimeout(() => displayBlock(), 2000);
 		controls.start({
 			y: "-100%",
 			transition: {duration: 1}
-		})
+		});
+		// Fire the displayBlock function after 2 seconds
+		setTimeout(() => displayBlock(), 2000);
 	};
 
+	// shows the div below
 	const displayBlock = () => {
-		setStyle("block")
+		setStyle("1")
 		console.log("done")
 	}
 
@@ -28,8 +34,11 @@ const Main = () => {
 				<button id="enter" onClick={animateName}>Enter</button>
 				<h4 id="role">Full Stack Developer.</h4>
 			</motion.div>
-			<div style={{display: style}}>
-				Hello
+			<div style={{
+				opacity: style,
+				transition: "0.6s ease-in"
+				}}>
+				<AboutMe />
 			</div>
 		</div>
 
