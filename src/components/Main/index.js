1a9f4de4dfa088f5b7af4cd935b39735fd085b87
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-12 18:17:35 -0600

    Added some flex styling and added in some motion.divs

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./style.css";
 
 class Main extends React.Component {
 	render() {
 		return(
-			<div className="block">
-				<h1 className="name">Sean</h1>
-				<h1 className="name">Belverstone.</h1>
-				<h4 id="role">Full Stack Developer.</h4>
+			<div className="container">
+				<motion.div animate={{}} className="nameBlock">
+					<h1 className="name">Sean</h1>
+					<h1 className="name">Belverstone.</h1>
+				</motion.div>
+				<motion.div animate={{}} className="roleBlock">
+					<h4 id="role">Full Stack Developer.</h4>
+				</motion.div>
 			</div>
+
+
 		)
 	}
 }
