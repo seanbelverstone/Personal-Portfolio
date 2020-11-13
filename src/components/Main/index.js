11b7d64c939d69215f0ce9c38e2dc6db271763b1
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-12 18:55:39 -0600

    using controls.start, added a button that causes the main screen to slide up on click

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import "./style.css";
 
-class Main extends React.Component {
-	render() {
-		return(
-			<div className="container">
-				<motion.div animate={{}} className="nameBlock">
-					<h1 className="name">Sean</h1>
-					<h1 className="name">Belverstone.</h1>
-				</motion.div>
-				<motion.div animate={{}} className="roleBlock">
-					<h4 id="role">Full Stack Developer.</h4>
-				</motion.div>
-			</div>
+const Main = () => {
+	const controls = useAnimation();
+
+	const animateName = (event) => {
+		controls.start({
+			y: "-100%",
+			transition: {duration: 1}
+		})
+	};
+
+	return(
+		<div className="container">
+			<motion.div id="mainBlock" animate={controls}>
+				<h1 className="name">Sean</h1>
+				<h1 className="name">Belverstone.</h1>
+				<button id="enter" onClick={animateName}>Enter</button>
+				<h4 id="role">Full Stack Developer.</h4>
+			</motion.div>
+		</div>
 
 
 		)
-	}
 }
 
 export default Main;
