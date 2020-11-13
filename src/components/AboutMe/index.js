5194cd57e08d662f9a68a9501f9b8fcff088adf5
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-13 13:38:27 -0600

    edited my picture, added some border radius on it and added an h2 for the title

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import me from "../../assets/images/me.jpg"
+import "./style.css";
 
 class AboutMe extends React.Component {
 	render() {
 		return(
-			<div>
-				<img src={me} alt="me" />
+			<div className="aboutMeSection">
+				<h2>From Chef to Coder</h2>
+				<img src={me} alt="me" id="me" />
 			</div>
 		)
 	}
