3e70bad6861cb3361d72ca91fcab3e7a38db4272
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-17 21:35:31 -0600

    Not using the plane, gave me too many errors. Opted for a --- line instead. Added on-hover functions to show pins and a line. Adding text

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -14,7 +14,7 @@ class AboutMe extends React.Component {
 					</Grid>
 					<Grid container justify="center" spacing={2}>
 						<Grid item xs={6}id="aboutText">
-							<p>As an Englishman who's spent most of his career in the catering industry, I've made a smooth transition over to web development. After completing a Full Stack Web Developer bootcamp at UT Austin, I am now proficient in React, Javascript, Node.js, Express, HTML & CSS, among other programs, libraries and frameworks.</p>
+							<p>As an Englishman who's spent most of his career in the catering industry, I've made a smooth transition over to web development. After completing a Full Stack Web Developer bootcamp at UT Austin in 2019, I am now proficient in <b>React</b>, <b>Javascript</b>, <b>Node.js</b>, <b>Express</b>, <b>MySQL</b>, <b>HTML & CSS</b>, among other programs, libraries and frameworks.</p>
 							<p>With a keen eye for designing front-end and a logical brain for tackling back-end, I'd be an essential asset to your team.</p>
 							<p>Continue below to see my story...</p>
 						</Grid>
--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,14 +1,49 @@
-import React from "react";
-import plane from "../../assets/images/bluePlane.png";
+import React, {useState} from "react";
+import { motion, useAnimation } from "framer-motion";
 import map from "../../assets/images/map.png";
+import pin from "../../assets/images/pin.png";
+import plane from "../../assets/images/blackPlane.png";
 import "./style.css";
 
 const Map = () => {
+
+	const [opacity, setOpacity] = useState("0");
+
+	const controls = useAnimation();
+
+	const pinsEnter = () => {
+		controls.start({
+			y: "15%",
+			opacity: 1,
+			transition: {duration: 0.5}
+		})	
+		showHideText("1");	
+	}
+
+	const pinsLeave = () => {
+		controls.start({
+			y: "-25%",
+			opacity: 0,
+			transition: {
+				duration: 0.5
+			}
+		})
+		showHideText("0");	
+
+	}	
+
+	const showHideText = (number) => {
+		setOpacity(number);
+	}
 	
 
 	return(
-		<div className="container">
-			<img id="map" src={map} alt="a world map"/>
+		<div className="mapContainer" onMouseEnter={pinsEnter} onMouseLeave={pinsLeave}>
+			<img id="map" src={map} alt="a world map" />
+			<motion.img id="pin1" src={pin} alt="a pin" animate={controls} initial={{opacity: 0}}/>
+			<motion.img id="pin2" src={pin} alt="a pin" animate={controls} initial={{opacity: 0}}/>
+			<div id="lines" style={{opacity: opacity}}>------</div>
+			<div id="journeyText" style={{opacity: opacity}}>I moved to Austin in 2018 to be with my wife, and we since  </div>
 		</div>
 	)
 }
