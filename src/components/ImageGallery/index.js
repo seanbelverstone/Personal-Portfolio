3b6b202541eb4362ebf9d89ec5672ba846779133
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-17 22:32:06 -0600

    Added in the images bit. Will do more removals tomorrow

--- src/components/ImageGallery/index.js
+++ src/components/ImageGallery/index.js
@@ -1,9 +1,83 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import blackPlane from "../../assets/images/blackPlane.png";
+import bluePlane from "../../assets/images/bluePlane.png";
+import meSuit from "../../assets/images/meSuit.jpg";
+import "./style.css";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+	overflow: 'hidden'
+  },
+  gridList: {
+    width: 500,
+    height: 450,
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
+  icon: {
+    color: 'white',
+  },
+}));
+
+// possibly save this elsewhere and import it so it looks nicer
+const tileData = [
+	{
+		img: blackPlane,
+		title: 'Black Plane',
+		author: 'mee',
+	},
+	{
+		img: bluePlane,
+		title: 'blue Plane',
+		author: 'mee',	
+	},
+	{
+		img: meSuit,
+		title: 'Me in a suit',
+		author: 'mee',
+	},
+];
 
 const ImageGallery = () => {
-	return(
-		<div>Images go here</div> 
-	)
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <GridList cellHeight={200} spacing={1} className={classes.gridList}>
+        {tileData.map((tile) => (
+          <GridListTile key={tile.img} cols={tile.featured ? 2 : 1} rows={tile.featured ? 2 : 1}>
+            <img src={tile.img} alt={tile.title} />
+            <GridListTileBar
+              title={tile.title}
+              titlePosition="top"
+              actionIcon={
+                <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
+                  <StarBorderIcon />
+                </IconButton>
+              }
+              actionPosition="left"
+              className={classes.titleBar}
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
 }
 
 export default ImageGallery;
