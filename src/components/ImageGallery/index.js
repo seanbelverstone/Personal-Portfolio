c81b35d60e3a8b3ed5c9e03cd0916f19774e549b
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-18 22:01:40 -0600

    Managed to grab the data-src that i wanted

--- src/components/ImageGallery/index.js
+++ src/components/ImageGallery/index.js
@@ -11,31 +11,48 @@ import "./style.css";
 
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-	overflow: 'hidden'
-  },
-  gridList: {
-    width: 500,
-    height: 450,
-    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-    transform: 'translateZ(0)',
-  },
-  titleBar: {
-    background:
-      'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-      'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  },
-  icon: {
-    color: 'white',
-  },
-}));
+
+	root: {
+		display: 'flex',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		overflow: 'hidden'
+	},
+	gridList: {
+		width: 500,
+		height: 450,
+		// Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+		transform: 'translateZ(0)',
+	},
+	titleBar: {
+		background:
+			'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
+			'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+		},
+		icon: {
+		color: 'white',
+		},
+	}	
+));
+
+
+// data-src={tile.img}
 
 
 const ImageGallery = () => {
-  const classes = useStyles();
+	const classes = useStyles();
+
+	const largeImage = (event) => {
+		// store the data-src attribute in imageTarget
+		let imageTarget = event.target.getAttribute("data-src");
+
+		// if imageTarget returns null, use the farthestViewportElement instead, which is the parent essentially.
+		if (!imageTarget) {
+			imageTarget = event.target.farthestViewportElement.getAttribute("data-src");
+			console.log(imageTarget);
+		}
+	}
+
 
   return (
     <div className={classes.root}>
@@ -49,12 +66,13 @@ const ImageGallery = () => {
 			  subtitle={tile.date}
               actionIcon={
 				//   maybe use this icon to pop a modal out and see the picture full screen?
-                <IconButton aria-label={`star ${tile.title}`} className={classes.icon}>
-                  <FontAwesomeIcon icon={faEye}/>
+                <IconButton aria-label={`star ${tile.title}`} className={classes.icon} onClick={largeImage} data-src={tile.img}>
+                  <FontAwesomeIcon icon={faEye} data-src={tile.img}/>
                 </IconButton>
               }
               actionPosition="left"
-              className={classes.titleBar}
+			  className={classes.titleBar}
+			  data-src={tile.img}
             />
           </GridListTile>
         ))}
