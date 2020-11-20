27edf5de55512e3b9577567637cd3e89b84f9532
Sean Belverstone <seanbelverstone@gmail.com>
2020-11-19 23:45:16 -0600

    Managed to save each one into state. Yaay!

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import TextField from '@material-ui/core/TextField';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import { makeStyles } from '@material-ui/core/styles';
@@ -6,7 +6,6 @@ import Grid from "@material-ui/core/Grid";
 import Button from '@material-ui/core/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
-
 import "./style.css";
 
 const useStyles = makeStyles((theme) => ({
@@ -18,24 +17,33 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const sendEmail = () => {
-	console.log("egg")
-}
 
 const ContactForm = () => {
-  const classes = useStyles();
+	const classes = useStyles();
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [message, setMessage] = useState("");
+
+	
+
+	const sendEmail = () => {
+		return console.log(`Name: ${name}, Email: ${email}, Phone: ${phone}, Message: ${message}`);
+	}
+
+
 
   return (
     <form className={classes.root} noValidate autoComplete="off" id="formWrapper">
 		<div className="formContainer">
 			<Grid item xs={12}>
-				<TextField id="outlined-basic" label="Name" variant="outlined" />
-				<TextField id="outlined-basic" label="Email" variant="outlined" />
-				<TextField id="outlined-basic" label="Phone" variant="outlined" />
+				<TextField id="outlined-basic" value={name} onChange={(event) => setName(event.target.value)} label="Name" variant="outlined" />
+				<TextField id="outlined-basic" value={email} onChange={(event) => setEmail(event.target.value)} label="Email" variant="outlined" />
+				<TextField id="outlined-basic" value={phone} onChange={(event) => setPhone(event.target.value)} label="Phone" variant="outlined" />
 			</Grid>
 			<Grid>
 				<p id="messageLabel">Enter your message below</p>
-				<TextareaAutosize id="outlined-basic" className="message" label="Message" variant="outlined" rowsMin={6} />
+				<TextareaAutosize id="outlined-basic" value={message} onChange={(event) => setMessage(event.target.value)} className="message" label="Message" variant="outlined" rowsMin={6} />
 			</Grid>
 			<Button
 				variant="contained"
